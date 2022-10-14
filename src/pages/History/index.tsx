5b02d80e0feb27534ce0346406ff6678b4f9a418
Leonardo Martins <leodsmartins@gmail.com>
2022-10-14 14:18:08 -0300

    Criação dos contextos

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,71 +1,75 @@
+import { useContext } from "react";
+import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
-  return (
-    <HistoryContainer>
-      <h1>Minhas tarefas</h1>
-        <HistoryList>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Tarefa</th>
-                        <th>Duração</th>
-                        <th>Início</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Nome da tarefa</td>
-                        <td>30 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nome da tarefa</td>
-                        <td>30 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nome da tarefa</td>
-                        <td>30 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nome da tarefa</td>
-                        <td>30 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nome da tarefa</td>
-                        <td>30 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nome da tarefa</td>
-                        <td>30 minutos</td>
-                        <td>Há 2 meses</td>
-                        <td>
-                            <Status statusColor="green">Concluído</Status>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-      </HistoryList>
-    </HistoryContainer>
-  );
+	const { cycles } = useContext(CyclesContext);
+
+	return (
+		<HistoryContainer>
+			<h1>Minhas tarefas</h1>
+			<HistoryList>
+				<table>
+					<thead>
+						<tr>
+							<th>Tarefa</th>
+							<th>Duração</th>
+							<th>Início</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Nome da tarefa</td>
+							<td>30 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Nome da tarefa</td>
+							<td>30 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Nome da tarefa</td>
+							<td>30 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Nome da tarefa</td>
+							<td>30 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Nome da tarefa</td>
+							<td>30 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+						<tr>
+							<td>Nome da tarefa</td>
+							<td>30 minutos</td>
+							<td>Há 2 meses</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</HistoryList>
+		</HistoryContainer>
+	);
 }
